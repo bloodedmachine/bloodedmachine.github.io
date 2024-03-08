19b325f1531f781be4e763e6a0c108d777050001
myx <104733415+myxchine@users.noreply.github.com>
2024-03-08 11:47:50 +0000

    Update pong.js

--- pong.js
+++ pong.js
@@ -28,7 +28,7 @@ const paddleWidth = ((window.innerHeight - 30) / (16/9)) * 0.3;
 const paddleHeight = paddleWidth * 0.333;
 let topPaddleX = canvas.width / 2 - paddleWidth / 2 + paddleHeight * 0.01;
 
-const ballSize = paddleWidth * 0.1;
+const ballSize = paddleWidth * 0.3;
 let ballX = canvas.width / 2;
 let ballY = 12 + canvas.height / 2;
 let ballSpeedX = 8;
