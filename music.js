34c5b2ae3d481eb907c49491cc4864717a896b2c
readyplayerone <michael.dozzantoz@gmail.com>
2024-03-12 00:49:24 +0000

    v

--- music.js
+++ music.js
@@ -1,8 +1,8 @@
 
 const songs = [
   { name: 'FASHION', artist: 'Britney Mason', albumCover: 'album.png', url: 'Britney-Manson-Fashion.mp3' },
-{ name: 'BACK TO ME', artist: 'ye', albumCover: 'album.png', url: 'Kanye West & Ty Dolla $ign - BACK TO ME (Lyrics) ft. Freddie Gibbs & Quavo (192 kbps).mp3' },
- { name: 'CARNIVAL', artist: 'ye', albumCover: 'album.png', url: 'CARNIVAL - Kanye West & Ty Dolla $ign (ft Playboi Carti & Rich The Kid) (lyrics) (192 kbps).mp3' },
+{ name: 'BACK TO ME', artist: 'Ye', albumCover: 'album.png', url: 'Kanye West & Ty Dolla $ign - BACK TO ME (Lyrics) ft. Freddie Gibbs & Quavo (192 kbps).mp3' },
+ { name: 'CARNIVAL', artist: 'Ye', albumCover: 'album.png', url: 'CARNIVAL - Kanye West & Ty Dolla $ign (ft Playboi Carti & Rich The Kid) (lyrics) (192 kbps).mp3' },
   
 ];
 
@@ -98,7 +98,7 @@ const audio = document.getElementById('audio');
 const seekSlider = document.getElementById('seekSlider');
 const songNameElement = document.getElementById('songName');
 const artistNameElement = document.getElementById('artistName');
-const albumCoverElement = document.getElementById('albumCover');
+//const albumCoverElement = document.getElementById('albumCover');
 
 function generateSongList(songs) {
   songList.innerHTML = '';
